b6feac49f1c2a94c2b89497ac4cf466cdc5acbaa
Ramtin Soltani <ramtinsoltani93@gmail.com>
2021-02-24 14:36:41 -0800

    "Singular commit"

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { Singular } from '@singular/core';
+import tsconfig from './tsconfig.json';
+import { profiles } from './server.config';
+
+Singular
+.registerAliases(tsconfig.compilerOptions.paths)
+.config('dev', profiles.dev)
+.config('prod', profiles.prod)
+.config('test', profiles.test)
+.launch();
---
+++ src/server.config.ts
@@ -0,0 +1,30 @@
+import { ServerConfig } from '@singular/core';
+
+export const profiles: { [name: string]: ServerConfig } = {
+
+  dev: {
+    port: 5000,
+    logRequestHeaders: true,
+    logResponseErrors: true
+  },
+
+  prod: {
+    https: true,
+    httpsPort: 5001,
+    httpsKey: 'path/to/key.pem',
+    httpsCert: 'path/to/cert.pem',
+    enableCors: true,
+    logRequestHeaders: true,
+    logResponseErrors: true
+  },
+
+  test: {
+    port: 5002,
+    writeLogsToFile: false,
+    logRequestHeaders: true,
+    logResponseErrors: true,
+    archiveLogs: false,
+    consoleLogLevels: 'all',
+  }
+
+};
